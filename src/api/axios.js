4eba783848c0a9c3575669d932e3bc780d1e4277
Anjali Sharma <anjalivce19@gmail.com>
2025-11-14 15:47:56 +0530

    url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", // backend API URL
+  baseURL: "https://crm-system-1-auch.onrender.com/api", // backend API URL
 
 });
 
